dedf12cc28576630e242a4451c1389b5ffcf72c2
manuK1777 <manuelkrapovickas@gmail.com>
2024-08-29 16:44:29 +0200

    Exercise 1 completed, builded reactive form with check boxes, added basic logic and styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,51 @@
 import { Component } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormBuilder,
+  FormControl,
+} from '@angular/forms';
+import { CommonModule, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  template: '<button class="btn btn-primary">Bootstrap Button</button>',
+  template: '',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, JsonPipe],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  budgetForm!: FormGroup;
 
+  selectedValues: number[] = [];
+  preuPressuposat: number = 0;
+
+  constructor(private fb: FormBuilder) {
+    this.budgetForm = this.fb.group({
+      seo: [false],
+      ads: [false],
+      web: [false],
+    });
+  }
+
+  onCheckboxChange(event: Event, value: number) {
+    const isChecked = (event.target as HTMLInputElement).checked;
+
+    if (isChecked) {
+      this.selectedValues.push(value);
+    } else {
+      const index = this.selectedValues.indexOf(value);
+      if (index > -1) {
+        this.selectedValues.splice(index, 1);
+      }
+    }
+
+    this.preuPressuposat = this.selectedValues.reduce(
+      (accumulator, currentValue) => {
+        return accumulator + currentValue;
+      },
+      0
+    );
+  }
 }
-
