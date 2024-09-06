15eb6e181f5c5edd37ef4e591d656614494e8d4a
manuK1777 <manuelkrapovickas@gmail.com>
2024-09-06 16:39:44 +0200

    Exercise 4 completed. Added unitary tests to budget service, totalWebPrice and totalServices functions

--- src/app/services/budget.service.spec.ts
+++ src/app/services/budget.service.spec.ts
@@ -13,4 +13,26 @@ describe('BudgetService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it ('should calculate the total web price', () => {
+    const budgetService = TestBed.inject(BudgetService);
+
+    const numPages = 2;
+    const numLang = 3;
+    const expectedResult = numPages * numLang * 30;
+
+    const result = budgetService.totalWebPrice(numPages, numLang);
+    expect(result).toBe(expectedResult);
+  
+});
+
+it ('should calculate the total services', () => {
+  const budgetService = TestBed.inject(BudgetService);
+
+  const selectedValues = [300, 400, 500];
+  const expectedResult = 1200;
+
+  const result = budgetService.totalServices(selectedValues);
+  expect(result).toBe(expectedResult);
+})
 });
